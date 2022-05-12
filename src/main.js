d0f221abcfef52b6ebd76eaa985ce59cf0089d9f
Pavel Yurchanka <pavel.yurchanka@oracle.com>
2022-05-12 13:54:37 +0300

    there are prettier lint left

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import {createApp} from 'vue';
-import App from '@/App.vue';
-
-const app = createApp(App);
-
-app.mount('#app');
+document.querySelector('#app').innerHTML = `
+	<div style="
+		height: 98vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	">
+		<div>
+			<h1 style="text-align: center">Приложение запущено</h1>
+			<h2
+				style="
+					white-space: pre-line;
+					text-align: center;
+				"
+			>Прежде чем начать, определись с технологией
+				Проект поддерживает React и Vue</h2>
+			<h2 style="text-align: center">Удачи)</h2>
+		</div>
+	</div>
+`
